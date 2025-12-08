48676a057df89498288b214060f035f3f0442be4
cznoemii <czinegenoemi303@gmail.com>
2025-12-08 12:21:47 +0100

    kész oldal

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -26,11 +26,47 @@
         }
 
         .page-wrapper {
-            background-color: #F1F9FF;
-            min-height: 100vh;
-            font-family: 'Inter', sans-serif;
-            display: flex;
-            flex-direction: column;
+            position: relative;
+            overflow: hidden;
+            z-index: 0;
+        }
+
+        .page-wrapper::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 27%;
+            height: 100%;
+            background:
+                linear-gradient(to left, rgba(241, 249, 255, 1), rgba(241, 249, 255, 0)),
+                repeating-linear-gradient(145deg,
+                    #19427A 0px,
+                    #19427A 80px,
+                    #F4FCFF 80px,
+                    #F4FCFF 120px);
+            opacity: 0.50;
+            pointer-events: none;
+            z-index: -1;
+        }
+
+        .page-wrapper::after {
+            content: "";
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 27%;
+            height: 100%;
+            background:
+                linear-gradient(to right, rgba(241, 249, 255, 1), rgba(25, 66, 122, 0)),
+                repeating-linear-gradient(145deg,
+                    #19427A 0px,
+                    #19427A 80px,
+                    #F4FCFF 80px,
+                    #F4FCFF 120px);
+            opacity: 0.50;
+            pointer-events: none;
+            z-index: -1;
         }
 
         .main-content {
@@ -39,6 +75,7 @@
             max-width: 1200px;
             margin: 0 auto;
             width: 100%;
+
         }
 
         .page-title {
@@ -47,34 +84,46 @@
             color: #322799;
             margin-bottom: 24px;
             text-align: center;
+            font-family: 'Karma';
+            color: #19427A;
+            font-size: 60px;
         }
 
         .content-placeholder {
-            background: #F1F9FF;
+
             border-radius: 12px;
             padding-top: none;
-            padding: 40px;
+
             /*box-shadow: 0 2px 8px rgba(0, 0, 0, 0.04);*/
             min-height: 400px;
+            padding-right: 21%;
+            ;
+            padding-left: 21%;
+            ;
 
         }
 
         .content-placeholder p {
             color: #666;
             font-size: 16px;
+            padding-right: 200px;
+            padding-left: 200px;
+
+
         }
 
         .mezők {
-
+            padding-right: 200px;
+            padding-left: 200px;
             width: 100%;
             padding: 12px 16px;
-
+            color: #19427A;
             border-radius: 8px;
             font-size: 16px;
         }
 
         .mez {
-
+            color: #19427A;
             width: 100%;
             padding: 12px 16px;
 
@@ -95,6 +144,9 @@
         h4 {
             text-align: right;
             color: #322799;
+            margin-left: 10px;
+            margin-right: 10px;
+            display: ;
         }
 
         .but {
@@ -106,10 +158,53 @@
         }
 
         button {
-            background-color: #322799;
+            background-color: #19427A;
             color: white;
             max-height: 50%;
             width: 200px%;
+            padding: 12px 24px;
+            font-size: 16px;
+            min-width: 200px;
+            margin-top: 50px;
+            float: center;
+
+            border: none;
+            border-radius: 10px;
+            font-weight: 600;
+            letter-spacing: 0.5px;
+            /*box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);*/
+            /*transition: transform 0.2s, box-shadow 0.2s;*/
+            /*display: block;*/
+        }
+
+        button:hover {
+            background: linear-gradient(135deg, #4F709B 0%, #19427A 100%);
+        }
+
+        .fiok {
+            color: #19427A;
+            text-align: center;
+            padding-top: 50px;
+        }
+
+        .reg {
+            text-decoration: none;
+            color: #322799;
+        }
+
+        .reg:hover {
+            color: #10094bff;
+
+        }
+
+        .pass {
+            text-align: right;
+            float: right;
+            color: #322799;
+        }
+
+        .pass:hover {
+            color: #10094bff;
         }
     </style>
 </head>
@@ -130,10 +225,10 @@
                     {{-- A munkatársak ide dolgozhatnak --}}
                 </div>
                 <div class="mez">
-                    <label for="form-label">Jelszó</label>
-                    <a href="/forgot-password">
-                        <h4>Elfelejtett jelszó?</h4>
-                    </a>
+
+                    <label for="form-label">Jelszó <a class="pass" href="/forgot-password">Elfelejtett
+                            jelszó?</a></label>
+
                     <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
                         placeholder="Jelszó" required>
                     @error('password')
@@ -143,6 +238,10 @@
                 <div class="but">
                     <a href="/"><button>Tovább</button></a>
                 </div>
+
+                <div class="fiok">
+                    Nincs még fiókod?<a class="reg" href="/register"><b>Regisztrálj!</b></a></div>
+
             </div>
         </main>
 
